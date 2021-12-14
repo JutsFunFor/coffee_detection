abbfe482d679946b5b4970996a4c3e624459b471
JutsFunFor <43553016+JutsFunFor@users.noreply.github.com>
2021-12-14 08:37:08 +0300

    Update NatsClient.py

--- NatsClient.py
+++ NatsClient.py
@@ -52,5 +52,9 @@ if __name__ == '__main__':
     loop = asyncio.get_event_loop()
     loop.create_task(NatsClient().send_msg('Hello, World!', loop))
     
-    loop.run_forever()
-    loop.close()
+    try:
+        loop.run_forever()
+    except Exception as err:
+        print(err)
+    finally:
+        loop.close()
